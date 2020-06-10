887be44346e2f30e6d59f7b714195fcb78395312
Jiang Yuxin <jyx11011@gmail.com>
2020-06-10 09:43:44 +0000

    Add a marker with info window

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -371,6 +371,21 @@ function initMap() {
     center: {lat: 1.352, lng: 103.8198},
     zoom: 12
   });
+
+  const marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: {lat: 1.35, lng: 103.8198},
+    label: 'A'
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+    content: 'Hello, world!'
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 }
 
 window.onload = () => {
